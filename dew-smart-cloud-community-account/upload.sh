53f62f6c512053e213b38880a52c20e90f4f1d12
Leo Wang <isWangzl@aliyun.com>
2019-11-06 20:14:46 +0800

    [ADD]增加上传脚本

---
+++ dew-smart-cloud-community-account/upload.sh
@@ -0,0 +1,2 @@
+scp ./target/generated-resources/appassembler/jsw/dew-account/lib/*.jar root@116.62.51.50:/opt/Dew/AccountSvrApplication/lib
+
---
+++ dew-smart-cloud-community-gateway/upload.sh
@@ -0,0 +1,2 @@
+scp ./target/generated-resources/appassembler/jsw/dew-gateway/lib/*.jar root@116.62.51.50:/opt/Dew/DewCommunityGateWay/lib
+
