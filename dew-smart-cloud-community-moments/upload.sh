fc611fdda52a2e5b47099ceb074bb8cc7358f3bb
LeoWang <LeoWang@inslabs.org>
2020-03-12 17:00:31 +0800

    [MOD]配置变更

--- dew-smart-cloud-community-moments/upload.sh
+++ dew-smart-cloud-community-moments/upload.sh
@@ -1,3 +1,3 @@
-scp ./target/generated-resources/appassembler/jsw/dew-mos/lib/*.jar root@116.62.51.50:/opt/Dew/DewMomentsSvrApplication/lib
+scp  -P 1113 ./target/generated-resources/appassembler/jsw/dew-mos/lib/*.jar root@116.62.51.50:/opt/Dews/DewMomentServerUnStableApplication/lib
 
 
