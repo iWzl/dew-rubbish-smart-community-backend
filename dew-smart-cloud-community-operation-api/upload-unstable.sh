154c63cb1875e133f799de337f7a4792a3804760
LeoWang <LeoWang@inslabs.org>
2020-04-11 17:07:04 +0800

    [ADD]OSS-API部署调整

---
+++ dew-smart-cloud-community-operation-api/upload-unstable.sh
@@ -0,0 +1,3 @@
+scp ./target/generated-resources/appassembler/jsw/dew-oss-api/lib/*.jar root@10.147.20.20:/opt/Dews/DewOperationApiSvrUnStableApplication/lib
+
+
---
+++ dew-smart-cloud-community-operation-api/upload.sh
@@ -0,0 +1,3 @@
+scp ./target/generated-resources/appassembler/jsw/dew-css/lib/*.jar root@116.62.51.50:/opt/Dew/DewCommunitySupportApplication/lib
+
+
